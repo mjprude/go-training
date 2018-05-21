81be8d06842f7f468461a3ac6b3ebe814f82b89b
mjprude <michaelprude@gmail.com>
2018-05-21 17:33:56 -0400

    Refactor slices

--- slices-4/sum.go
+++ slices-4/sum.go
@@ -2,10 +2,35 @@
 package main
 
 // Sum adds together an Array of integers
-func Sum(numbers [5]int) int {
+func Sum(numbers []int) int {
 	sum := 0
-	for i := 0; i < 5; i++ {
-		sum += numbers[i]
+	for _, number := range numbers {
+		sum += number
 	}
 	return sum
+}
+
+// SumAll summs multiple slices
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
+}
+
+// SumAllTails sums the tails (all non-0 indexed items in a slice) of passed slices
+func SumAllTails(slices ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range slices {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			sums = append(sums, Sum(numbers[1:]))
+		}
+	}
+	return sums
 }
